3bb765c9e0805da45955627f32fd34bf8a2b3889
Eclecticdynastye <bklyngal1994@gmail.com>
2018-11-09 13:01:04 -0500

    bcrypt and validations added to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  
+validates :email, :password, :username, presence: true
+validates :email, uniqueness: { case_sensitive: false }
+validates :email, format: { with: VALID_EMAIL_REGEX }
+validates :password, length: { minimum: 8 }
+
 end
